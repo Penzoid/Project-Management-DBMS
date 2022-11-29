e0a92c967a8cff28861178866914780821be727e
Akbasniwal <akbasniwal2907@gmail.com>
2022-11-29 18:57:23 +0530

    home page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,51 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./common.css";
-import img1 from "../images/1.jpg";
-import img2 from "../images/2.jpg";
-import img3 from "../images/3.jpg";
+// import img1 from "../images/1.jpg";
+// import img2 from "../images/2.jpg";
+// import img3 from "../images/3.jpg";
 
 export default function Home() {
+  useEffect(() => {
+    const scriptTag = document.createElement("script");
+
+    scriptTag.src = "https://notion2embed.com/v1/m20dcxfp?h1=no";
+    scriptTag.async = true;
+
+    document.body.appendChild(scriptTag);
+    return () => {
+      document.body.removeChild(scriptTag);
+    };
+  }, []);
   return (
-    <div className="container carousel slide my-5" data-bs-ride="false">
-      <div className="card mb-3">
+    <div
+      className="container"
+      data-bs-ride="false"
+      style={{ marginBottom: "-10px", marginTop: "-55px" }}
+    >
+      {/* <div className="card mb-3 cardhead">
         <img src={img1} className="card-img-top cropped1" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
+          <p className="card-text" style={{ fontSize: "25px" }}>
+            Welcome to the Student-Teacher Project Evaluation Management System.
           </p>
         </div>
       </div>
-      <div className="card">
+      <div className="card mb-3 cardhead">
         <img src={img2} className="card-img-bottom cropped1" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
+          <p className="card-text" style={{ fontSize: "25px" }}>
+            This is the one stop platform where student can form teams and
+            submit their projects and assignments.
           </p>
         </div>
       </div>
-      <div className="card">
+      <div className="card mb-3 cardhead">
         <img src={img3} className="card-img-bottom cropped1" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
+          <p className="card-text" style={{ fontSize: "25px" }}>
+            Teachers can evaluate projects submitted by student and assign
+            grades and make appropriate remarks for the same.
           </p>
         </div>
-      </div>
+      </div> */}
+      <div id="notion2embed" class="cleanslate"></div>
     </div>
   );
 }
